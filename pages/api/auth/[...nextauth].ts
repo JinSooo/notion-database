69352fa0499d4ee67448d7e1e9ffe237e7e8f50f
JinSo <kimjinso@qq.com>
2023-02-22 11:41:34 +0800

    feat: NextAuth Email登录

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,18 +1,29 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
-// import EmailProvider from 'next-auth/providers/email'
+import EmailProvider from 'next-auth/providers/email'
+import prisma from '@/lib/prisma'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID ?? '',
 			clientSecret: process.env.GITHUB_SECRET ?? '',
 		}),
-		// EmailProvider({
-		// 	server: process.env.EMAIL_SERVER,
-		// 	from: process.env.EMAIL_FROM,
-		// }),
+		EmailProvider({
+			server: process.env.EMAIL_SERVER,
+			from: process.env.EMAIL_FROM,
+		}),
 	],
+	callbacks: {
+		session: async ({ session, token, user }) => {
+			if (session?.user) {
+				session.user.id = user.id
+			}
+			return session
+		},
+	},
 }
 
 export default NextAuth(authOptions)
